73094028b9a3be947ca152f64377861bc099848a
Jiri Cincura <jiri@x2develop.com>
2014-04-05 11:10:58 +0200

    Simple program.

--- ConfigureAwaitChecker/Program.cs
+++ ConfigureAwaitChecker/Program.cs
@@ -1,29 +1,37 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using ConfigureAwaitChecker.Lib;
 
 namespace ConfigureAwaitChecker
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var testFile = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Code.cs");
-            var checker = new Checker(testFile);
-            Console.WriteLine(checker.DebugListTree());
+	class Program
+	{
+		static int Main(string[] args)
+		{
+			if (args.Count() != 1)
+				return 1;
+			if (string.IsNullOrWhiteSpace(args[0]))
+				return 2;
+			if (!File.Exists(args[0]))
+				return 3;
 
-            foreach (var item in checker.Check())
-            {
-                if (!item.HasConfigureAwaitFalse)
-                {
-                    Console.WriteLine("ERROR: Missing 'ConfigureAwait(false)' for await on line {0} column {1}.", item.Line, item.Column);
-                }
-                else
-                {
-                    Console.WriteLine("Good. Found 'ConfigureAwait(false)' for await on line {0} column {1}.", item.Line, item.Column);
-                }
-            }
-        }
-    }
+			var checker = new Checker(args[0]);
+
+			foreach (var item in checker.Check())
+			{
+				if (!item.HasConfigureAwaitFalse)
+				{
+					Console.Error.WriteLine("ERROR: Missing 'ConfigureAwait(false)' for await on line {0} column {1}.", item.Line, item.Column);
+				}
+				else
+				{
+					Console.Out.WriteLine("Good. Found 'ConfigureAwait(false)' for await on line {0} column {1}.", item.Line, item.Column);
+				}
+			}
+
+			return 0;
+		}
+	}
 }
