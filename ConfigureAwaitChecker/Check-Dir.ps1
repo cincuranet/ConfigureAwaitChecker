56a849159d6423671d3357cbbcc0b01506fa29ec
Jiri Cincura <jiri@x2develop.com>
2014-04-25 12:46:56 +0200

    Using Write-Output.

--- ConfigureAwaitChecker/Check-Dir.ps1
+++ ConfigureAwaitChecker/Check-Dir.ps1
@@ -1,6 +1,6 @@
 ﻿param([Parameter(Mandatory=$True,Position=1)][string]$directory)
 Get-ChildItem $directory -Filter *.cs -Recurse | foreach { 
 	$fullName = $_.FullName
-	Write-Host "Checking $fullName."
+	Write-Output "Checking $fullName."
 	.\ConfigureAwaitChecker.exe $fullName
 }
