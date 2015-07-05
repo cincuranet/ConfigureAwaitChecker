940d55b0664d0e5aa2a4e3a5f67e475ccce3afd6
Jiri Cincura <jiri@x2develop.com>
2015-07-05 09:39:59 +0200

    Extracted exit codes.

---
+++ ConfigureAwaitChecker.Console/ExitCodes.cs
@@ -0,0 +1,11 @@
+﻿namespace ConfigureAwaitChecker.Console
+{
+	static class ExitCodes
+	{
+		public const int OK = 0;
+		public const int Error = 1;
+		public const int TooFewArguments = 101;
+		public const int ArgumentEmpty = 102;
+		public const int FileNotFound = 103;
+	}
+}
