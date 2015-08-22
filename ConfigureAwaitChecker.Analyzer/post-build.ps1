8d65e13a2d4daa6f86613d472d2729d5a521305c
Jiri Cincura <jiri@x2develop.com>
2015-08-22 09:58:22 +0200

    Renamed NuGet package name.

--- ConfigureAwaitChecker.Analyzer/post-build.ps1
+++ ConfigureAwaitChecker.Analyzer/post-build.ps1
@@ -1,7 +1,7 @@
-﻿try {
+try {
 	$version = (gi ConfigureAwaitChecker.dll).VersionInfo.ProductVersion
 	echo "Building package with version $version."
-	& '..\..\..\packages\NuGet.CommandLine.2.8.5\tools\NuGet.exe' pack ConfigureAwaitChecker.nuspec -Version $version -OutputDirectory .
+	& '..\..\..\packages\NuGet.CommandLine.2.8.5\tools\NuGet.exe' pack ConfigureAwaitChecker.Analyzer.nuspec -Version $version -OutputDirectory .
 }
 catch {
 	exit 1
