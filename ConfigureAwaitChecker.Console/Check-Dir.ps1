5035ce56b0d2a2aff15ea113c0e40df1b1ca7cd8
Jiri Cincura <jiri@x2develop.com>
2018-06-08 15:04:33 +0200

    Console part no longer available.

--- ConfigureAwaitChecker.Console/Check-Dir.ps1
+++
@@ -1,6 +0,0 @@
-﻿param([Parameter(Mandatory=$True,Position=1)][string]$directory)
-Get-ChildItem $directory -Filter *.cs -Recurse | foreach { 
-	$fullName = $_.FullName
-	Write-Output "Checking $fullName."
-	.\ConfigureAwaitChecker.Console.exe $fullName
-}
